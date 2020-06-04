421b7b6c725d7b20d77cfe528e5d6d244f58ce71
Nazmul1216 <nazmul.kabir1216@gmail.com>
2020-06-03 22:30:56 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,30 @@
 # Add your code here
+class Dog
+    @@all = []
+    def initialize(name)
+        @name=name
+        self.save
+    end 
+    def name=(name)
+        @name=name
+    end
+    def name
+        @name 
+    end
+    def self.all
+        @@all
+    end
+    def self.clear_all
+        @@all=[]
+    end
+    def save
+        @@all << self
+    end
+    def self.print_all
+        string=""
+        @@all.each do |dog|
+            string = string + dog.name + "\n"
+        end 
+       puts string
+    end
+end 
